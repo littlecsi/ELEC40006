783c813f0bed5219bfa3971e58b0bdaf11694344
Lian Choi <littlecsi27@gmail.com>
2021-06-05 00:07:05 +0100

    [add] - update main

--- 40006/main.cpp
+++ 40006/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 
+#include "store.h"
+
 int main() {
-	return 0;
+    std::vector<Component> circuit;
+    Option option;
+    std::string fileName;    
+
+    std::cout << "Circuit File : ";
+    std::cin >> fileName;
+
+    ReadFile(&circuit, &option, fileName);
+
+    for (int i = 0; i < circuit.size(); i++) {
+        std::cout << circuit[i].designator << " ";
+        std::cout << circuit[i].node0 << " ";
+        std::cout << circuit[i].node1 << " ";
+        std::cout << circuit[i].node2 << " ";
+        std::cout << circuit[i].node3 << " ";
+        std::cout << circuit[i].value << std::endl;
+    }
+
+    return 0;
 }
