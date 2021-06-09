6b67e76d33b732efcc9dd6b374d6bd24090039f6
Lian Choi <littlecsi27@gmail.com>
2021-06-09 17:44:36 +0100

    [fix] - fixed a lot of stuff on SSEM

--- code/canalysis.h
+++ code/canalysis.h
@@ -1,4 +1,7 @@
-#define PI 3.141592653589793238463
+#define M_PI 3.141592653589793238463
+#define ELEC_Vt 25e-03
+#define ELEC_Is 0.3e-06
+#define ELEC_Vbe 0.7
 
 #include <iostream>
 #include <vector>
@@ -7,6 +10,8 @@
 #include <cmath>
 #include <math.h>
 #include <string.h>
+
+typedef std::vector<std::vector<double>> Mat;
 
 using namespace std::complex_literals;
 
@@ -22,6 +27,20 @@ struct Option {
     std::string startFreq;
     std::string endFreq;
 };
+
+// -------------------- List of functions
+void ReadFile(std::vector<Component>*, Option*, std::string);
+void PrintCircuit(std::vector<Component>, Option);
+std::string FindOutputNode(std::vector<Component>);
+std::vector<Component> SmallSignalEquivalent(std::vector<Component>, int);
+double CapCond(std::string, int);
+double IndCond(std::string, int);
+double ResCond(std::string, int);
+double DioCond(std::string, int);
+double ConvertUnit(std::string value);
+std::vector<std::string> ConvertNode(std::vector<std::string>);
+double ftow(int);
+// ------------------------------------------------------------
 
 void ReadFile(std::vector<Component>* circuit, Option* option, std::string filename) {
 
@@ -116,19 +135,22 @@ std::vector<Component> SmallSignalEquivalent(std::vector<Component> circuit, int
 
     for (int i = 0; i < circuit.size(); i++) {
         Component comp;
+
         if (circuit[i].designator[0] == 'V') {
+            // Voltage Sources are open circuits.
             comp.designator = "";
             comp.value = "0";
 
             SSEM.push_back(comp);
         }
         else if (circuit[i].designator[0] == 'I') {
+            // Current Sources are open circuits.
             continue;
         }
         else if (circuit[i].designator[0] == 'R') {
             comp.designator = circuit[i].designator;
             comp.nodes = circuit[i].nodes;
-            comp.value = ResCond(circuit[i].value, freq);
+            comp.value = std::to_string(ResCond(circuit[i].value, freq));
 
             SSEM.push_back(comp);
         }
@@ -136,16 +158,25 @@ std::vector<Component> SmallSignalEquivalent(std::vector<Component> circuit, int
             comp.designator = circuit[i].designator;
             comp.nodes = circuit[i].nodes;
             comp.value = std::to_string(CapCond(circuit[i].value, freq));
+
+            SSEM.push_back(comp);
         }
         else if (circuit[i].designator[0] == 'L') {
             comp.designator = circuit[i].designator;
             comp.nodes = circuit[i].nodes;
             comp.value = std::to_string(IndCond(circuit[i].value, freq));
+
+            SSEM.push_back(comp);
         }
         else if (circuit[i].designator[0] == 'D') {
             comp.designator = circuit[i].designator;
             comp.nodes = circuit[i].nodes;
-            comp.value = std::to_string(DioCond(circuit[i].value, freq));
+            comp.value = ELEC_Vt / (ELEC_Is * exp(ELEC_Vbe / ELEC_Vt));
+
+            SSEM.push_back(comp);
+        }
+        else if (circuit[i].designator[0] == 'Q') {
+            
         }
     }
     return SSEM;
@@ -163,10 +194,9 @@ double ResCond(std::string value, int freq) { // Find Resistor Conductance
     return 1.0 / ConvertUnit(value);
 }
 
-double DioCond(std::string value, int freq) { // Find Resistor Conductance
-    double Vt = 0.25e-03;
-    double Id = 0.3e-06 * exp(0.7 / Vt); // Assuming forward BIAS, V = 0.7V
-    return Id / Vt;
+double DioCond(std::string value) { // Find Resistor Conductance
+    double Id = ELEC_Is * exp(0.7 / ELEC_Vt); // Assuming forward BIAS, V = 0.7V
+    return Id / ELEC_Vt;
 }
 
 double ConvertUnit(std::string value) {
@@ -195,13 +225,28 @@ double ConvertUnit(std::string value) {
     else if (value[value.size() - 1] == 'G') {
         multiplier = 1e+09; 
     }
-    else if (value.substr(value.size() - 3, value.size() - 1) == "Meg") {
+    else if (value.substr(value.size() - 3, 3) == "Meg") {
         multiplier = 1e+06;
         return (double)(multiplier * std::stoi(value.substr(0, value.size() - 3)));
     }
     return multiplier * std::stod(value.substr(0, value.size() - 1));
 }
 
+std::vector<std::string> ConvertNode(std::vector<std::string> nodes) {
+    std::vector<std::string> result;
+
+    for (int i = 0; i < nodes.size(); i++) {
+        if (nodes[i] == "0") {
+            result.push_back("0");
+        }
+        else if (nodes[i][0] == 'N') {
+            result.push_back(nodes[i].substr(1, 3));
+        }
+    }
+
+    return result;
+}
+
 double ftow(int freq) { // frequency to Angular freq
-    return 2 * PI * freq;
+    return 2 * M_PI * freq;
 }
